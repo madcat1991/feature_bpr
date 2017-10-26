64bfcebd09000991bfd6f06b50d99e644fbea150
Tural Gurbanov <madcat1991@gmail.com>
2017-10-26 14:16:27 +0200

    Updates to the data reader and preprocessor

--- data_functions/pairwise.py
+++ data_functions/pairwise.py
@@ -1,10 +1,4 @@
-import numpy as np
 import pandas as pd
-
-
-UID_COL = 0
-IID_A_COL = 1
-IID_B_COL = 2
 
 
 def normailze_uids_and_iids(df, uid_cat=None, iid_cat=None):
@@ -19,16 +13,3 @@ def normailze_uids_and_iids(df, uid_cat=None, iid_cat=None):
     df.iid_b = df.iid_b.astype('category', categories=iid_cat.categories).cat.codes
 
     return df, uid_cat, iid_cat
-
-
-def read_pw_data_as_X_and_y(pw_df, random_state=42, sample_negative=True, negative_p=0.5):
-    y = np.ones(pw_df.shape[0])
-
-    if sample_negative:
-        pw_df = pw_df.sample(frac=1, random_state=random_state)
-        n_neg = int(pw_df.shape[0] * negative_p)
-        neg_ids = np.random.choice(pw_df.shape[0], n_neg, False)
-        pw_df.iloc[neg_ids, [IID_A_COL, IID_B_COL]] = pw_df.iloc[neg_ids, [IID_B_COL, IID_A_COL]].values
-        y[neg_ids] = 0
-
-    return pw_df.values, y
