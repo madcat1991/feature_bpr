699805ac35f2e4082cc4aa5b395f39a7ca302ffb
Tural Gurbanov <madcat1991@gmail.com>
2017-10-28 18:07:51 +0200

    New metric, roc_auc_score_avg_by_users

--- metrics.py
+++ metrics.py
@@ -1,5 +1,5 @@
 import numpy as np
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, roc_auc_score
 
 
 def accuracy_score_avg_by_users(y_true, y_pred, uids):
@@ -8,3 +8,11 @@ def accuracy_score_avg_by_users(y_true, y_pred, uids):
         idx = np.where(uids == uid)[0]
         uid_accuracies.append(accuracy_score(y_true[idx], y_pred[idx]))
     return np.mean(uid_accuracies)
+
+
+def roc_auc_score_avg_by_users(y_true, y_pred, uids):
+    uid_aucs = []
+    for uid in np.unique(uids):
+        idx = np.where(uids == uid)[0]
+        uid_aucs.append(roc_auc_score(y_true[idx], y_pred[idx]))
+    return np.mean(uid_aucs)
